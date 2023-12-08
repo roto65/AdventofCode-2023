110f36e0751f0bc7730d928574b6ab2e66c94fd8
Alessandro Rotondo <antoniorotondo65@gmail.com>
2023-12-08 12:05:03 +0100

    Added gcd and lcm functions

--- shared.hpp
+++ shared.hpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 
-std::vector<std::string> readFile (std::string path) {
+std::vector<std::string> readFile(std::string path) {
     std::vector<std::string> text;
 
-    std::ifstream file (path);
+    std::ifstream file(path);
     if (file.is_open()) {
         std::string line;
-        while(std::getline(file,line)) {
+        while (std::getline(file, line)) {
             text.push_back(line);
         }
     }
@@ -14,14 +14,24 @@ std::vector<std::string> readFile (std::string path) {
     return text;
 }
 
-std::vector<std::string> split (const std::string &s, char delim = ' ') {
+std::vector<std::string> split(const std::string &s, char delim = ' ') {
     std::vector<std::string> result;
-    std::stringstream ss (s);
+    std::stringstream ss(s);
     std::string item;
 
-    while (getline (ss, item, delim)) {
-        result.push_back (item);
+    while (getline(ss, item, delim)) {
+        result.push_back(item);
     }
 
     return result;
+}
+
+long long gcd(long long a, long long b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+long long lcm(long long a, long long b) {
+    return (a / gcd(a, b)) * b;
 }
